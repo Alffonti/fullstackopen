1399e80ce99b59969fbdfcf6602c422d0b0fa6c4
Alf Fonti <alffonti@gmail.com>
2023-01-02 11:27:27 -0500

    3.22 phonebook-backend

---
+++ part3/README.md
@@ -0,0 +1,33 @@
+# Part 3 - Node.js and Express
+
+In this part our focus shifts towards the backend, that is, towards implementing functionality on the server side of the stack. We will implement a simple REST API in Node.js by using the Express library, and the application's data will be stored in a MongoDB database. At the end of this part, we will deploy our application to the internet.
+
+## Exercises
+
+- Phonebook
+
+## Tools
+
+- Node.js
+- Express
+- [VS Code REST Client plugin](https://marketplace.visualstudio.com/items?itemName=humao.rest-client)
+- Nodemon
+- MongoDB Atlas
+- Mongoose
+- ESlint
+- Cyclic
+
+## Resources
+
+- [NodeJS Docs](https://nodejs.org/docs/latest-v8.x/api/)
+- [Understanding the npm dependency model](https://lexi-lambda.github.io/blog/2016/08/24/understanding-the-npm-dependency-model/)
+- [Semantic versioning](https://docs.npmjs.com/about-semantic-versioning)
+- [Express API reference](https://expressjs.com/en/4x/api.html)
+- [Routing - Express](https://expressjs.com/en/guide/routing.html)
+- [Middleware - Express](https://expressjs.com/en/guide/using-middleware.html)
+- [Representational State Transfer (REST) - Roy Fielding's dissertation](https://www.ics.uci.edu/~fielding/pubs/dissertation/rest_arch_style.htm)
+- [Richardson Maturity Model](https://martinfowler.com/articles/richardsonMaturityModel.html)
+- [REST APIs must be hypertext-driven](https://roy.gbiv.com/untangled/2008/rest-apis-must-be-hypertext-driven)
+- [RFC 9110 HTTP Standard](https://www.rfc-editor.org/rfc/rfc9110.html)
+- [Mongoose Docs](https://mongoosejs.com/docs/guide.html)
+- [ESlint Docs](https://eslint.org/docs/latest/)
--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -2,110 +2,125 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 
+require('dotenv').config()
+
+const Person = require('./models/person')
+
 const app = express()
 
-let persons = [
-	{
-		id: 1,
-		name: 'Arto Hellas',
-		number: '040-123456',
-	},
-	{
-		id: 2,
-		name: 'Ada Lovelace',
-		number: '39-44-5323523',
-	},
-	{
-		id: 3,
-		name: 'Dan Abramov',
-		number: '12-43-234345',
-	},
-	{
-		id: 4,
-		name: 'Mary Poppendieck',
-		number: '39-23-6423122',
-	},
-]
-
-morgan.token('data', function (req, res) {
-	return JSON.stringify(req.body)
-})
+morgan.token('data', request => JSON.stringify(request.body))
 
 app.use(express.json())
 
 app.use(
-	morgan(
-		':method :url :status :res[content-length] - :response-time ms :res[content-length] :data'
-	)
+  morgan(
+    ':method :url :status :res[content-length] - :response-time ms :res[content-length] :data'
+  )
 )
 
 app.use(cors())
 
+app.use(express.static('build'))
+
 app.get('/info', (request, response) => {
-	response.send(
-		`
+  Person.find({}).then(persons => {
+    response.send(
+      `
 			<p>Phonebook has info for ${persons.length} people</p>
 			<p>${new Date()}</p>
 		`
-	)
+    )
+  })
 })
 
-const generateId = (min, max) => {
-	min = Math.ceil(min)
-	max = Math.floor(max)
-	return Math.floor(Math.random() * (max - min + 1) + min) // The maximum is inclusive and the minimum is inclusive
-}
+app.post('/api/persons', (request, response, next) => {
+  const body = request.body
 
-app.post('/api/persons', (request, response) => {
-	const body = request.body
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: 'name or number missing',
+    })
+  }
 
-	if (!body.name || !body.number) {
-		return response.status(400).json({
-			error: 'name or number missing',
-		})
-	}
+  // const isName = Person.exists({ name: body.name })
+  // console.log(isName)
+  // if (isName) {
+  // 	return response.status(400).json({
+  // 		error: 'name must be unique',
+  // 	})
+  // }
 
-	const isName = persons.find(person => person.name === body.name)
-	if (isName) {
-		return response.status(400).json({
-			error: 'name must be unique',
-		})
-	}
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+  })
 
-	const person = {
-		id: generateId(1, Number.MAX_SAFE_INTEGER),
-		name: body.name,
-		number: body.number,
-	}
-
-	persons = persons.concat(person)
-	response.json(person)
+  person
+    .save()
+    .then(savedPerson => response.json(savedPerson))
+    .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-	const id = Number(request.params.id)
-	persons = persons.filter(person => person.id !== id)
+app.delete('/api/persons/:id', (request, response, next) => {
+  const id = request.params.id
 
-	response.status(204).end()
+  Person.findByIdAndDelete(id)
+    .then(() => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
-app.get('/api/persons/:id', (request, response) => {
-	const id = Number(request.params.id)
-	const person = persons.find(person => person.id === id)
+app.get('/api/persons/:id', (request, response, next) => {
+  const id = request.params.id
 
-	if (person) {
-		response.json(person)
-	} else {
-		response.statusMessage = `A person with an id of ${id} doesn't exist`
-		response.status(404).end()
-	}
+  Person.findById(id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.statusMessage = `A person with an id of ${id} doesn't exist`
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
 app.get('/api/persons', (request, response) => {
-	response.json(persons)
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
 })
 
-const PORT = 3001
+app.put('/api/persons/:id', (request, response, next) => {
+  const { name, number } = request.body
+
+  Person.findByIdAndUpdate(
+    request.params.id,
+    { name, number },
+    { new: true, runValidators: true, context: 'query' }
+  )
+    .then(updtatedPerson => {
+      response.json(updtatedPerson)
+    })
+    .catch(error => next(error))
+})
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
+
+const PORT = process.env.PORT
 app.listen(PORT, () => {
-	console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
